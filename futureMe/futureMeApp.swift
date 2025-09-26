ec848fe2bd8c9b6ddffec6642329b69d522b69f1
Ivan Wu <yupingwu38@gmail.com>
2025-09-26 19:13:17 +0900

    Initial Commit

---
+++ futureMe/futureMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  futureMeApp.swift
+//  futureMe
+//
+//  Created by 吳堉平 on 2025/09/26.
+//
+
+import SwiftUI
+
+@main
+struct futureMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
